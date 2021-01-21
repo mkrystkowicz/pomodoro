d0af3bb0aba838705599ffcae4a7d8a0b5425fbd
Mateusz <krissie515@gmail.com>
2021-01-21 18:22:26 +0100

    add basic store functions

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,45 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    clocks: [
+      {
+        clockType: "pomodoro-clock",
+        amount: 10000,
+        paused: false,
+        isRunning: false
+      },
+      {
+        clockType: "short-break",
+        amount: 10000,
+        paused: false,
+        isRunning: false
+      },
+      {
+        clockType: "long-break",
+        amount: 10000,
+        paused: false,
+        isRunning: false
+      }
+    ]
+  },
+  getters: {
+    getPomodoroTime: state => clockType => {
+      const clock = state.clocks.find(clock => clock.clockType === clockType);
+
+      return clock.amount;
+    }
+  },
+  mutations: {
+    decrementTime: (state, clockType) => {
+      const clock = state.clocks.find(clock => clock.clockType === clockType);
+      if (clock.amount === 0) return 0;
+      else return (clock.amount -= 1000);
+    }
+  },
+  actions: {
+    handleClock({ commit }, payload) {
+      commit("decrementTime", payload);
+    }
+  }
 });
